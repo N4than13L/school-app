20ab4b6a685c3e11e7f0511f4c1c5b49154d0219
Nathaniel <natharevolution@gmail.com>
2023-09-21 16:30:28 -0400

    creando metodo auth

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,11 @@ use App\Models\Student;
 
 Route::get('/', function () {
 
-    $students = Student::all();
-    foreach ($students as $student) {
-        echo "Estudiante: " . $student->name . ' ' . $student->surname . ', edad: ' . $student->age;
-        echo "<br/>";
-        echo "usuario: " . $student->Users->name . ' ' . $student->Users->surname;
 
-        echo "<br/>";
-        echo "Maestro: " . $student->Teachers->name;
-        echo "Tutor: " . $student->Tutors->name;
-        echo   "<hr/>";
-    }
-    die();
 
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
